fa450be0fae237d0f9e13604ac0b12545c8c196e
alucas- <alucas-@student.42.fr>
2017-07-25 03:33:20 +0200

    io changes

--- cli.c
+++ cli.c
@@ -11,14 +11,14 @@
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdalign.h>
 
 #include "bsq.h"
 
 int		main(int ac, char **av)
 {
-	printf("size: %zu, align: %zu\n", sizeof(t_square), alignof(t_square));
-	printf("size: %zu, align: %zu\n", sizeof(t_map), alignof(t_map));
-	printf("size: %zu, align: %zu\n", sizeof(t_bsq), alignof(t_bsq));
+	t_uint32 size;
+
+	t_lbuff root = {&root, &root, 0, 0, 0};
+	printf("%s", bsq_read_infoline(0, &size, &root));
 	return (0);
 }
